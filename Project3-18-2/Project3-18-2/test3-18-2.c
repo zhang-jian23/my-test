c58f2c591b8371645f092118d2f78064d2745362
zhang-jian23_global <919602517@qq.com>
2020-03-21 21:33:21 +0800

    提交代码

---
+++ Project3-18-2/Project3-18-2/test3-18-2.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//int main()
+//{
+//	int num = 1;
+//	while (num<=100)
+//	{
+//		if (num % 2 == 1)
+//			printf("  %d", num);
+//		num++;
+//	}
+//
+//	return 0;
+//
+//}
+int main()
+{
+	int num = 1;
+
+	while (num <= 100)
+
+	{
+		printf("  %d", num );
+	num = num + 2;
+	}
+	return 0;
+}
