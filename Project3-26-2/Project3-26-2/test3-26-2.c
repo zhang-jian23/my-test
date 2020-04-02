50625076b3f1099e646428e6488dfb1d728e854f
zhang-jian23_global <919602517@qq.com>
2020-04-02 22:16:36 +0800

    提交代码

---
+++ Project3-26-2/Project3-26-2/test3-26-2.c
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+int is_leap_year(int year)
+{
+	if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+
+	}
+}
+int main()
+{   
+
+	int year = 0;
+
+	for (year = 1000; year <= 2000; year++)
+	{
+
+		if (is_leap_year(year) == 1)
+		{
+			printf("%5d", year);
+			
+		}
+	}
+
+	return 0;
+}
