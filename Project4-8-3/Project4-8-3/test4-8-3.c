b4fffe9377fc2055f9f31846ac07b26bcac4bf6f
zhang-jian23_global <919602517@qq.com>
2020-04-15 22:19:47 +0800

    打印菱形

---
+++ Project4-8-3/Project4-8-3/test4-8-3.c
@@ -0,0 +1,45 @@
+#define _CRT_SECURE_NO_WARNINGS 
+
+#include<stdio.h>
+int main()
+{
+	int line = 0;
+	int i = 0;
+	scanf("%d", &line);
+
+	for (i = 0; i < line; i++)
+	{
+		int j = 0;
+		for (j = 0; j < line - 1 - i; j++)
+		{
+			printf(" ");
+		}
+		for (j = 0; j < 2 * i + 1; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+
+	for (i = 0; i < line - 1; i++)
+	{
+		int j = 0;
+
+		for (j = 0; j <= i; j++)
+		{
+			printf(" ");
+		}
+		
+		
+		for (j = 0; j < 2 * (line - 1 - i) - 1; j++)
+
+		{
+			printf("*");
+		}
+		
+		printf("\n");
+
+	}
+	
+	return 0;
+}
