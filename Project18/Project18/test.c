39fb30a87c846990f9b5b4f59569a5c1b00f2e75
zhang-jian23_global <919602517@qq.com>
2020-05-18 23:13:16 +0800

    练习

---
+++ Project18/Project18/test.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+main()
+{
+	int i = 5;
+	void palin(int n);
+	printf("\40:");
+	palin(i);
+	printf("\n");
+}
+void palin(n)
+int n;
+{
+	char next;
+	if (n <= 1)
+	{
+		next = getchar();
+		printf("\n\0:");
+		putchar(next);
+	}
+	else
+	{
+		next = getchar();
+		palin(n - 1);
+		putchar(next);
+	}
+}
