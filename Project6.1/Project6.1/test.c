088012f5463093a75d7cc4281a6947f76fa04fa9
zhang-jian23_global <919602517@qq.com>
2020-06-01 23:00:15 +0800

    提交代码

---
+++ Project6.1/Project6.1/test.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include<stdio.h>
+int main()
+{
+	int len;
+	char *str[20];
+	printf("please input a string:\n");
+	scanf("%s", str);
+	len = length(str);
+	printf("the string has %d characters.", len);
+}
+length(p)
+char *p;
+{
+	int n;
+	n = 0;
+	while (*p != 0)
+	{
+		n++;
+		p++;
+	}
+	return n;
+}
